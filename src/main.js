f77fd9d19a633b26009abebfb0d57dbe5681477c
MiAnMy <mikolaj.andrasiak@ncbj.gov.pl>
2020-08-18 18:09:56 +0200

    Delete Event dispatcher

--- src/main.js
+++ src/main.js
@@ -2,11 +2,9 @@ import Vue from "vue";
 import App from "./App.vue";
 import Buefy from "buefy";
 import "buefy/dist/buefy.css";
-import dispatcher from "./dispatcher.js";
 import "@mdi/font/css/materialdesignicons.css";
 Vue.use(Buefy);
 Vue.config.productionTip = false;
-window.Event = dispatcher;
 
 new Vue({
   render: (h) => h(App),
